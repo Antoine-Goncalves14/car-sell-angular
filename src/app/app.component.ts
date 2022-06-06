b1a704b66e2b70bd49c4f620b0765196e9ee851f
Antoine-Goncalves-ISC-France <antoine.goncalves-isc.france@ibm.com>
2022-06-06 11:50:35 +0200

    two way binding" "

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  text = '';
 
-  onClickButton(): void {
-    this.text = 'Hello World !';
-  }
+  text = 'Hello World!';
 
 }

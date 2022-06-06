be96546dec727e33d659ed041d11ea2404417bc0
Antoine-Goncalves-ISC-France <antoine.goncalves-isc.france@ibm.com>
2022-06-06 11:36:01 +0200

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  paragrapheId = 0;
+  text = '';
+
+  onClickButton(): void {
+    this.text = 'Hello World !';
+  }
+
 }

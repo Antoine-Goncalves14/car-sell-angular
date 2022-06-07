5c14355c06f07d87f6c512bb0cbdd0db605e7bfc
Antoine-Goncalves-ISC-France <antoine.goncalves-isc.france@ibm.com>
2022-06-07 15:39:48 +0200

    installation AngularFire et firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { registerLocaleData } from '@angular/common';
 import localFr from '@angular/common/locales/fr';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment';
 
 registerLocaleData(localFr);
 
@@ -22,6 +27,10 @@ registerLocaleData(localFr);
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
   ],
   providers: [
     {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'car-sell-1676e',
+    appId: '1:808922340616:web:ace7ba211f387149fa60bd',
+    storageBucket: 'car-sell-1676e.appspot.com',
+    apiKey: 'AIzaSyDGuQuAYhBqP1UFjkCTBtXOKI2xwlFzRTo',
+    authDomain: 'car-sell-1676e.firebaseapp.com',
+    messagingSenderId: '808922340616',
+    databaseUrl: 'https://car-sell-1676e-default-rtdb.europe-west1.firebasedatabase.app',
+  },
   production: true
 };

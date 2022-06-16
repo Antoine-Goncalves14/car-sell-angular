ce924a18f542eb6c75453d78a9372b17df4242dc
Antoine-Goncalves-ISC-France <antoine.goncalves-isc.france@ibm.com>
2022-06-16 14:43:59 +0200

    Gestion de l'état de connexion

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from '../interfaces/user';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+
+  currentUserSubscription!: Subscription;
+  currentUser!: User;
 
   title = 'CarSell';
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+  ) { }
 
   ngOnInit(): void {
+    this.currentUserSubscription = this.authService.currentUserSubject.subscribe({
+      next: user => this.currentUser = <User>user,
+      error: console.error,
+    });
+  }
+
+  getTitle(): string {
+    return this.title;
+  }
+
+  ngOnDestroy(): void {
+    this.currentUserSubscription.unsubscribe();
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  currentUserSubject = new BehaviorSubject<User | null>(null);
 
   constructor(
     private auth: AngularFireAuth,
-  ) { }
+  ) {
+    this.auth.onAuthStateChanged(user => {
+      this.currentUserSubject.next(user);
+    }, console.error);
+  }
 
   signUpUser(email: string, password: string): Promise<any> {
     return new Promise((resolve, reject) => {
